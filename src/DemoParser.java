aff2d5876ba681c90029219d87865a083360baef
Frank Qin <qzh@cs.washington.edu>
2020-10-28 05:29:56 -0700

    Implement expression parsing

--- src/DemoParser.java
+++ src/DemoParser.java
@@ -5,6 +5,7 @@ import AST.Visitor.*;
 import java_cup.runtime.Symbol;
 import java_cup.runtime.ComplexSymbolFactory;
 import java.util.*;
+import java.beans.Expression;
 import java.io.*;
 
 public class DemoParser {
@@ -19,20 +20,17 @@ public class DemoParser {
             // replace p.parse() with p.debug_parse() in the next line to see
             // a trace of parser shift/reduce actions during parsing
             root = p.parse();
-	    // We know the following unchecked cast is safe because of the
-	    // declarations in the CUP input file giving the type of the
-	    // root node, so we suppress warnings for the next assignment.
-	    @SuppressWarnings("unchecked")
-            List<Statement> program = (List<Statement>)root.value;
-            for (Statement statement: program) {
-                statement.accept(new PrettyPrintVisitor());
-                System.out.print("\n");
-            }
+            // We know the following unchecked cast is safe because of the
+            // declarations in the CUP input file giving the type of the
+            // root node, so we suppress warnings for the next assignment.
+            @SuppressWarnings("unchecked")
+            Exp program = (Exp) root.value;
+            program.accept(new ASTPrintVisitor());
+            System.out.print("\n");
         } catch (Exception e) {
             // yuck: some kind of error in the compiler implementation
             // that we're not expecting (a bug!)
-            System.err.println("Unexpected internal compiler error: " + 
-                               e.toString());
+            System.err.println("Unexpected internal compiler error: " + e.toString());
             // print out a stack dump
             e.printStackTrace();
         }
